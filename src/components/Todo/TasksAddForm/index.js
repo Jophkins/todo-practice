97853032f594e7c9b7d9567ef80294cbdff1cafc
Jopkins <sergebrew@gmail.com>
2022-02-23 22:19:10 +0300

    AddForm component prepered to adding tasks

--- src/components/Todo/TasksAddForm/index.js
+++ src/components/Todo/TasksAddForm/index.js
@@ -1,10 +1,29 @@
+import {useState} from "react";
 
 
 const TasksAddForm = () => {
+
+  const [userInput, setUserInput] = useState('');
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserInput('')
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit(event)
+    }
+  }
+
   return(
     <div className="taskAddForm">
-      <form>
-        <input type="text"/>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} onKeyDown={handleKeyPress} type="text" value={userInput} placeholder="Task text here"/>
         <button>Add</button>
       </form>
     </div>
